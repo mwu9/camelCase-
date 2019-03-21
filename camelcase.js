8370fe9f98b9ac0e8d68cd4057e898d7bd69d648
May <mwu9@buffalo.edu>
2019-03-21 11:01:17 -0400

    Update camelcase.js

--- camelcase.js
+++ camelcase.js
@@ -30,3 +30,24 @@ function camelCase(str) {
   //the split method allows us to split a string into an array 
   let str = "Hi there how are you?"
   console.log(str.split(" "));
+
+
+  function camelCase(sentence){
+    let result = ''; //make an empty string for the loop to stack the words
+    
+    let stringArray = []; //make an empty array that contains every word in an array 
+    stringArray = sentence.split(" "); 
+    
+    for (let i= 0;i < stringArray.length; i++){
+      if (i === 0){
+        result = stringArray[i][0].toLowerCase() + stringArray[i].slice(1);
+      } else {
+        result = result + stringArray[i][0].toUpperCase() + stringArray[i].slice(1);
+      }
+    }
+    return result;
+    
+  };
+  
+  
+  console.log(camelCase('Hello there tiny people')); 
